36f6207119702216f8d1208e9a7b7b99711e3a9a
л�� <1972524359@qq.com>
2022-07-13 13:01:04 +0800

    初始化

---
+++ src/test/java/com/xx/GraduationDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
