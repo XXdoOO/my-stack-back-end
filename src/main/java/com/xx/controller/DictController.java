47bc66ce0699a2d96dcd286c4fd5fa8d4d170878
25826610264444218 <1972524359@qq.com>
2023-01-10 14:02:17 +0800

    优化字典管理

--- src/main/java/com/xx/controller/DictController.java
+++ src/main/java/com/xx/controller/DictController.java
@@ -1,5 +1,6 @@
 package com.xx.controller;
 
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.xx.pojo.dto.DictDTO;
@@ -8,6 +9,7 @@ import com.xx.service.DictService;
 import com.xx.util.MyResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -45,20 +47,23 @@ public class DictController {
 
     @ResponseBody
     @GetMapping("dictData")
-    public MyResponse getDictData(@RequestBody DictDTO dto) {
-        PageHelper.startPage(dto.getPageNum(), dto.getPageSize());
-        return MyResponse.success(new PageInfo<>(dictService.getDictData(dto)));
+    public MyResponse getDictData(DictDTO dto) {
+        if (StringUtils.isNotBlank(dto.getDictName())) {
+            PageHelper.startPage(dto.getPageNum(), dto.getPageSize());
+            return MyResponse.success(new PageInfo<>(dictService.getDictData(dto)));
+        }
+        return MyResponse.error();
     }
 
     @ResponseBody
     @PostMapping("dictData")
-    public MyResponse postDictData(@RequestBody DictDTO dto) {
+    public MyResponse postDictData(@Validated @RequestBody DictDTO dto) {
         return dictService.postDictData(dto) ? MyResponse.success() : MyResponse.fail();
     }
 
     @ResponseBody
     @PutMapping("dictData")
-    public MyResponse putDictData(@RequestBody DictDTO dto) {
+    public MyResponse putDictData(@Validated @RequestBody DictDTO dto) {
         return dictService.putDictData(dto) ? MyResponse.success() : MyResponse.fail();
     }
 
--- src/main/java/com/xx/pojo/dto/DictDTO.java
+++ src/main/java/com/xx/pojo/dto/DictDTO.java
@@ -2,8 +2,11 @@ package com.xx.pojo.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class DictDTO extends PageDTO{
+    @NotBlank
     private String dictName;
     private String label;
     private String value;
