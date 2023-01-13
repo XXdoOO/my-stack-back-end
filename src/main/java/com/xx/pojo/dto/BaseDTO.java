5ffec40a77cfc10a0d9f547ed4d9b5f73817c2e7
25826610264444218 <1972524359@qq.com>
2023-01-13 14:06:09 +0800

    优化

--- src/main/java/com/xx/pojo/dto/BaseDTO.java
+++ src/main/java/com/xx/pojo/dto/BaseDTO.java
@@ -18,12 +18,12 @@ abstract class BaseDTO{
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private final Date[] updateTime = new Date[]{null, null};
+    private Date[] updateTime = new Date[]{null, null};
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private final Date[] createTime = new Date[]{null, null};
-    private Long createBy;
+    private Date[] createTime = new Date[]{null, null};
+    private String createBy;
     private Integer pageNum = 1;
     private Integer pageSize = 10;
     private String orderBy = "id";
